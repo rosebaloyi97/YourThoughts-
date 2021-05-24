678454cc7cb7b12b36c471b26a747ed628b17e1a
rosebaloyi97 <rosebaloyi97@gmail.com>
2021-05-24 11:52:14 +0200

    queries

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,18 @@
 const database =require('./database');
 
+//getting all the diary posted
+
+exports.GetAllPost=(req, res)=>{
+    database.query('SELECT topic, message FROM newDiary', (err, rows, fields)=>{
+        if(!err){
+            res.json(rows);
+        }else{
+            throw err;
+        }
+    });
+}
+
+//posting a new diary
 exports.NewDiary=(req, res)=>{
     database.query('INSERT INTO newDiary(topic, message) VALUES (?, ?)', 
     [req.body.topic, req.body.message], (err, rows, fields)=>{
@@ -9,4 +22,14 @@ exports.NewDiary=(req, res)=>{
             throw err;
         }
     });
+}
+
+exports.DeleteDiary = (req , res)=>{
+    database.query('DELETE FROM newDiary WHERE diary_id = :diary_id ', (err, rows , fields)=>{
+        if(!err){
+            res.json(rows);
+        }else{
+            throw err;
+        }
+    })
 }
--- server/route.js
+++ server/route.js
@@ -2,8 +2,14 @@ const express =require('express');
 const controller =require('./controller');
 const route =express.Router();
 
+//getting all diary posted
+route.get("/AllDiaries", controller.GetAllPost);
+
 //writing a new diary
 route.post("/NewDiary", controller.NewDiary);
+
+//delete diary
+route.delete("/deleteDiary/:id", controller.DeleteDiary);
 
 
 module.exports =route;

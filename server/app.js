e6d0299c5b3f765c46f81a3913ccf81efa3d9d6a
rosebaloyi97 <rosebaloyi97@gmail.com>
2021-05-24 10:40:41 +0200

    routes implemented

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express =require('express');
 const bodyParser =require('body-parser');
 const cors =require('cors')
 const database = require('./database');
-
+const route =require('./route');
 //initializing
 const app= express();
 const port = 5000;
@@ -11,6 +11,7 @@ const port = 5000;
 //middlewares
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/Diary', route);
 
 //server
 app.listen(port,()=>console.log(`Server running at port: ${port}`));
